665702cde07912b04f558b9d60b7f2dcaa2fa9d7
nhandh <hello@world.com>
2015-11-20 14:15:27 -0500

    Add take photo and take video. Done with all basic tasks. Will need to make the UI look more beautiful.

--- app/src/main/java/com/example/nhdo/lecturehelper/MainActivity.java
+++ app/src/main/java/com/example/nhdo/lecturehelper/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.nhdo.lecturehelper;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,7 +9,10 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     DBHelper mydb;
 
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_TAKE_VIDEO = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.main_tv_current_course);
         current_time = new CourseTime(0, 0);
         mydb = new DBHelper(this);
-
-        // TODO: change this to Update function that get called by (change in database OR after each minute)
 
     }
 
@@ -48,6 +54,55 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "The photo is saved!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (requestCode == REQUEST_TAKE_VIDEO) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "The video is saved!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void addPhoto(View view) {
+        final String PICTURE_FORMAT = ".jpg";
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = LectureHelperUtils.getEmptyFileWithStructuredPath
+                    (current_course_name, PICTURE_FORMAT);
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    public void addVideo(View view) {
+        final String VIDEO_FORMAT = ".mp4";
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            File videoFile = LectureHelperUtils.getEmptyFileWithStructuredPath
+                    (current_course_name, VIDEO_FORMAT);
+            if (videoFile != null) {
+                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(videoFile));
+                startActivityForResult(takeVideoIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
 
     public void addNote(View view) {
